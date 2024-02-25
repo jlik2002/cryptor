8cdad7ec01d602ab3a9737ca7c5c15fb8e90784a
jlik <>
2024-02-25 18:43:34 +0400

    add first test api from go doc

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	r.GET("/hello/:name", func(c *gin.Context) {
+		name := c.Param("name")
+		c.JSON(200, gin.H{
+			"message": "Hello " + name,
+		})
+	})
+	r.Run(":8070")
+}
